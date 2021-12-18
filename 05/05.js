8d74fccae682efa8a823515f3e53d789c8d039e7
Nuria <soriano.nuria@gmail.com>
2021-12-18 13:29:43 +0100

    day 5 part 2

--- 05/05.js
+++ 05/05.js
@@ -3,7 +3,8 @@ const parseInput = require("../parseInput");
 function solveStep1(input) {
   const data = parseInput(input);
 
-  /* huge array because I thought the area would be 10x10 based on the example and I'm too lazy to change it */
+  /* huge array because I thought the area would be 10x10
+  based on the example and I'm too lazy to change it now */
   const field = Array.from({ length: 1000 }, () =>
     Array.from({ length: 1000 }, () => 0)
   );
@@ -14,8 +15,8 @@ function solveStep1(input) {
     let [x2, y2] = end.split(",").map(Number);
 
     if (x1 === x2) {
-      const start = y2 > y1 ? y1 : y2;
-      const stop = y2 > y1 ? y2 : y1;
+      const start = Math.min(y1, y2);
+      const stop = Math.max(y1, y2);
 
       for (let i = start; i <= stop; i++) {
         field[i][x1]++;
@@ -35,7 +36,58 @@ function solveStep1(input) {
   return field.flat().filter((n) => n > 1).length;
 }
 
-function solveStep2(input) {}
+function solveStep2(input) {
+  const data = parseInput(input);
+
+  /* huge array because I thought the area would be 10x10
+  based on the example and I'm too lazy to change it now */
+  const field = Array.from({ length: 1000 }, () =>
+    Array.from({ length: 1000 }, () => 0)
+  );
+
+  data.forEach((entry) => {
+    const [start, end] = entry.split(" -> ");
+    let [x1, y1] = start.split(",").map(Number);
+    let [x2, y2] = end.split(",").map(Number);
+
+    if (x1 === x2) {
+      const start = Math.min(y1, y2);
+      const stop = Math.max(y1, y2);
+
+      for (let i = start; i <= stop; i++) {
+        field[i][x1]++;
+      }
+      return;
+    }
+
+    if (y1 === y2) {
+      const start = Math.min(x1, x2);
+      const stop = Math.max(x1, x2);
+
+      for (let i = start; i <= stop; i++) {
+        field[y1][i]++;
+      }
+      return;
+    }
+
+    let minX = Math.min(x1, x2);
+    let maxX = Math.max(x1, x2);
+    let minY = Math.min(y1, y2);
+    let maxY = Math.max(y1, y2);
+
+    if ((x1 > x2 && y1 > y2) || (x1 < x2 && y1 < y2)) {
+      for (let x = minX, y = minY; x <= maxX; x++, y++) {
+        field[y][x]++;
+      }
+    } else {
+      for (let x = minX, y = maxY; x <= maxX; x++, y--) {
+        field[y][x]++;
+      }
+    }
+  });
+
+  return field.flat().filter((n) => n > 1).length;
+}
 
 module.exports = {
   solveStep1,
--- 05/test.js
+++ 05/test.js
@@ -6,8 +6,8 @@ it("first step should return 5", () => {
   expect(result).toBe(5);
 });
 
-xit("second step should return 1924", () => {
+it("second step should return 12", () => {
   const result = solveStep2("/05/values-example.txt");
 
-  expect(result).toBe(1924);
+  expect(result).toBe(12);
 });
